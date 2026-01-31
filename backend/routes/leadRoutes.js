64193a00ed86ca0a4b0d260a03cd09c2da3eb6c6
Araveetimahendra <mahendraaraveeti2006@gmail.com>
2026-01-31 12:16:51 +0530

    FUTURE_FS_02

---
+++ backend/routes/leadRoutes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Lead = require("../models/Lead");
+
+const router = express.Router();
+
+// Save a new lead
+router.post("/", async (req, res) => {
+  const lead = new Lead(req.body);
+  await lead.save();
+  res.json({ message: "Lead saved" });
+});
+
+// Get all leads
+router.get("/", async (req, res) => {
+  const leads = await Lead.find();
+  res.json(leads);
+});
+
+module.exports = router;
+// DELETE lead using GET (for browser testing)
+router.get("/delete/:id", async (req, res) => {
+  await Lead.findByIdAndDelete(req.params.id);
+  res.send("Lead deleted");
+});
